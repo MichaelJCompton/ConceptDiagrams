4dab84980a4d342f2a852961c6f72b3bc8345fe0
MichaelJCompton <username@users.noreply.github.com>
2015-09-02 11:05:44 +1000

    initial commit

---
+++ src/main/java/org/ontologyengineering/conceptdiagrams/web/client/ConceptDiagrams.java
@@ -0,0 +1,12 @@
+package org.ontologyengineering.conceptdiagrams.web.client;
+
+import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.*;
+
+/**
+ * Created by Michael on 2/09/2015.
+ */
+public class ConceptDiagrams implements EntryPoint {
+    public void onModuleLoad() {
+    }
+}
